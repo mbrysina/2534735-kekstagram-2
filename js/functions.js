769059d03862539941533587e9449513edbe9961
mkozarezova <rita.kozarezova@gmail.com>
2024-12-03 22:48:22 +0300

    d3

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,9 @@
-eslint-disable-next line;
+// eslint-disable-next-line
 function isLengthValid(str, maxLength) {
   return str.length <= maxLength;
 }
 
-eslint-disable-next line;
+// eslint-disable-next-line
 function isPalindrome(str) {
   const cleanedStr = str.toLowerCase().replaceAll(' ', '');
   const reversedStr = cleanedStr.split('').reverse().join('');
