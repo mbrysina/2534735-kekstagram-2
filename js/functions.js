47b0db27c3949c26e604fb6f680b3d526677e6bd
mkozarezova <rita.kozarezova@gmail.com>
2024-12-03 21:46:07 +0300

    дом1.1

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,12 @@
 function isLengthValid(str , maxLength) {
   return str.length <= maxLength;
 }
+console.log(isLengthValid('проверяемая строка', 20)); // true
+console.log(isLengthValid('проверяемая строка', 18)); // true
+console.log(isLengthValid('проверяемая строка', 10)); // false
 
 function isPalindrome(str) {
   const cleanedStr = str.toLowerCase().replaceAll(' ', '');
   const reversedStr = cleanedStr.split('').reverse().join('');
   return cleanedStr === reversedStr;
 }
-isPalindrome('Топот');
