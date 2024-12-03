3e1ad88de2ba78d9d928a43199e939ebcb3ac2a1
mkozarezova <rita.kozarezova@gmail.com>
2024-12-03 22:45:08 +0300

    d2

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,11 @@
+eslint-disable-next line;
 function isLengthValid(str, maxLength) {
   return str.length <= maxLength;
 }
 
+eslint-disable-next line;
 function isPalindrome(str) {
   const cleanedStr = str.toLowerCase().replaceAll(' ', '');
   const reversedStr = cleanedStr.split('').reverse().join('');
   return cleanedStr === reversedStr;
 }
-
-const example = "А роза упала на лапу Азора";
-const maxLength = 50;
-
-const lengthValid = isLengthValid(example, maxLength);
-console.log(`Is length valid: ${lengthValid}`);
-
-const palindromeCheck = isPalindrome(example);
-console.log(`Is palindrome: ${palindromeCheck}`);
