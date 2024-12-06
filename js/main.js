ec3ae1222c3562bfa2e2697616e8324a5b1060cb
mkozarezova <rita.kozarezova@gmail.com>
2024-12-06 10:59:09 +0300

    d5

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+import { getRandomInt, getRandomElement } from './util.js';
 
-function getRandomElement(arr) {
-  return arr[getRandomInt(0, arr.length - 1)];
-}
+const MIN_AVATAR_ID = 1;
+const MAX_AVATAR_ID = 6;
+const MAX_PHOTOS = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MAX_COMMENTS = 30;
 
-function generateComments(numberOfComments) {
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = ['Артём', 'Светлана', 'Евгений', 'Ксения', 'Максим', 'Ольга', 'Дмитрий', 'Анна'];
+
+export function generateComments(numberOfComments) {
   const comments = [];
-  const messages = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-
-  const names = ['Артём', 'Светлана', 'Евгений', 'Ксения', 'Максим', 'Ольга', 'Дмитрий', 'Анна'];
 
   for (let i = 0; i < numberOfComments; i++) {
     const commentId = i + 1;
     const comment = {
       id: commentId,
-      avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
-      message: getRandomElement(messages),
-      name: getRandomElement(names)
+      avatar: `img/avatar-${getRandomInt(MIN_AVATAR_ID, MAX_AVATAR_ID)}.svg`,
+      message: getRandomElement(MESSAGES),
+      name: getRandomElement(NAMES)
     };
     comments.push(comment);
   }
@@ -33,22 +35,19 @@ function generateComments(numberOfComments) {
   return comments;
 }
 
-function generatePhotos() {
+export function generatePhotos() {
   const photos = [];
 
-  for (let i = 1; i <= 25; i++) {
+  for (let i = 1; i <= MAX_PHOTOS; i++) {
     const photo = {
       id: i,
       url: `photos/${i}.jpg`,
       description: `Описание фотографии номер ${i}`,
-      likes: getRandomInt(15, 200),
-      comments: generateComments(getRandomInt(0, 30))
+      likes: getRandomInt(MIN_LIKES, MAX_LIKES),
+      comments: generateComments(getRandomInt(0, MAX_COMMENTS))
     };
     photos.push(photo);
   }
 
   return photos;
 }
-
-generatePhotos();
-
